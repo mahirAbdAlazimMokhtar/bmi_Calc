4bb4201bc7164aaf52bf203fa669b7c97f4c6212
Mahir Molhtar <mahr.bwb@gmail.com>
2022-07-15 18:43:35 +0300

    build result screen

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
-  const ResultPage({Key? key}) : super(key: key);
+  const ResultPage(
+      {Key? key,
+      required this.result,
+      required this.isAge,
+      required this.isMale})
+      : super(key: key);
+
+  final double result;
+
+  final int isAge;
+  final bool isMale;
+
+  String get resultPhrase {
+    String resultText = '';
+    if (result >= 30) {
+      resultText = 'Obese';
+    } else if (result > 25 && result < 30) {
+      resultText = 'OverWeight';
+    } else if (result >= 18.5 && result <= 24.9) {
+      resultText = 'Normal';
+    } else {
+      resultText = 'Thin';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      appBar: AppBar(
+        title: const Text(
+          'Result',
+        ),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text('Gender: ${isMale ? 'Male' : 'Female'}',
+              style: Theme.of(context).textTheme.headline2),
+          Text('Result: ${result.toStringAsFixed(1)}',
+              style: Theme.of(context).textTheme.headline2),
+          Text('Healthiness: $resultPhrase',
+              style: Theme.of(context).textTheme.headline2),
+          Text('Age: $isAge', style: Theme.of(context).textTheme.headline2),
+        ],
+      )),
     );
   }
 }
